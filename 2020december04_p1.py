fd0e65cf9c99bd86452c86cb25412ffadf4f8345
csteiiin <tietan@seburai.de>
2020-12-04 21:30:02 +0100

    [SOLVED] - 2020 December 04

--- 2020december04_p1.py
+++ 2020december04_p1.py
@@ -1,48 +1,58 @@
 import os
 
 input_file = "2020december_passports.txt"
-#input_file = "input_temp.txt"
-persons = [{}]
-person_data = []
-with open(input_file, 'r') as file:
-    for line in file:
-        person_data.append(line)
 
-buffer_var = ""
-person_strings = []
-for row in range(len(person_data)):
-    if person_data[row] == "\n":
-        person_strings.append(buffer_var)
+passports_raw = []
+passports_in_strings = []
+passports = [{}]
+passports_counter = 0
+
+# extract passport data per person as a single string
+with open(input_file, 'r') as file:
+    buffer_var = ""
+    for line in file:
+        passports_raw.append(line)
+    passports_raw.append("\n")
+
+for row in range(len(passports_raw)):
+    # new line is used to separate passports
+    if passports_raw[row] == "\n" or passports_raw[row] == "\n":
+        passports_in_strings.append(buffer_var)
         buffer_var = ""
     else:
-        person_data[row] = person_data[row].replace("\n"," ")
-        buffer_var += person_data[row]
+        # data between two empty new lines (passports)
+        # can also contain a newline character @the end of the line
+        buffer_var += passports_raw[row].replace("\n"," ")
 
-ids = 0
-for string in person_strings:
-    persons.append({})
+for string in passports_in_strings:
+    passports.append({})
+    # whitespaces separate keys
     fields = string.split()
     for properties in fields:
-        prop = properties.split(":")
-        dict_entry = {prop[0]:prop[1]}
-        print(ids)
-        persons[ids].update(dict_entry)
-    ids += 1
+        properties = properties.split(":")
+        passports[passports_counter].update({properties[0]:properties[1]})
+    passports_counter += 1
 
 #remove last empty element
-persons.pop()
+passports.pop()
 
 #debug variables
 members = 0
 invalid = 0
 valid_passports = 0
+
 #detect if anything is missing
-for person_dict_data in persons:
+for entry in passports:
     members += 1
-    if ("byr" not in person_dict_data) or ("iyr" not in person_dict_data) or ("eyr" not in person_dict_data) or ("hgt" not in person_dict_data) or ("hcl" not in person_dict_data) or ("ecl" not in person_dict_data) or ("pid" not in person_dict_data):
-        print("invalid")
+    if (("byr" not in entry) or
+        ("iyr" not in entry) or
+        ("eyr" not in entry) or
+        ("hgt" not in entry) or
+        ("hcl" not in entry) or
+        ("ecl" not in entry) or
+        ("pid" not in entry)):
         invalid += 1
     else:
-        print("valid")
         valid_passports += 1
-print(f'{person_id}:{members}:{valid_passports}:{invalid}')
+
+print(f'Members:{members}\nValid Passports:{valid_passports}\nInvalid Passports:{invalid}')
