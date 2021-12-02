01f4d3f8b34eb087d412dc0d180da9ff94e65d97
HOHENSTEIN Christian <Christian.Hohenstein@akka.eu>
2021-12-02 07:56:52 +0100

    2021 December 02 Part 2 solved

--- 2021/2021_02.py
+++ 2021/2021_02.py
@@ -1,15 +1,18 @@
-counter = 0
 forward = 0
 level = 0
+aim = 0
 with open("2021/2021_02.txt", 'r') as file:
     for line in file:
         instructions = line.split()
         if instructions[0] == "forward":
             forward += int(instructions[1])
+            level += (aim * int(instructions[1]))
         elif instructions[0] == "down":
-            level += int(instructions[1])
+            # level += int(instructions[1])
+            aim += int(instructions[1])
         elif instructions[0] == "up":
-            level -= int(instructions[1])
+            # level -= int(instructions[1])
+            aim -= int(instructions[1])
         else:
             print("error")
     print(level*forward)
