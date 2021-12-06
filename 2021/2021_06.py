8b5c62d24230283c05418a6c1323671a096dcd30
HOHENSTEIN Christian <Christian.Hohenstein@akka.eu>
2021-12-06 10:18:51 +0100

    2021 December 06 Part 2 solved

--- 2021/2021_06.py
+++ 2021/2021_06.py
@@ -1,4 +1,6 @@
-CHECK_DATE = 80
+CHECK_DATE_1 = 80
+CHECK_DATE_2 = 256
+
 def readInput(filename):
     inp = []
     with open(filename, 'r') as fi:
@@ -16,12 +18,47 @@ def nextDay(fishList, day):
     day += 1
     return fishList, day
 
+def countAge(fishList):
+    #       0  1  2  3  4  5  6  7  8
+    ages = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for el in fishList:
+        ages[el] +=1
+    return ages
+
 def part1():
     # lantern list , day
-    lanterns = [readInput("2021/2021_06.txt"), 0]
-    while lanterns[1] < CHECK_DATE:
+    lanterns = [readInput("2021/test.txt"), 0]
+    while lanterns[1] < CHECK_DATE_1:
         lanterns = nextDay(lanterns[0],lanterns[1])
     print(len(lanterns[0]), sep="\n")
 
+def shiftAges(ageList):
+    newList = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for i in range(len(ageList)):
+        # new born lanterns equal the amount of
+        # last shift zero day lanterns
+        if i == 8:
+            newList[i] = ageList[0]
+        # 6 day olds are the sum of new born and last birth giving ones
+        elif i == 6:
+            newList[i] = ageList[i+1] + ageList[0]
+        # rest can simply be shifted from last day to today
+        else:
+            newList[i] = ageList[i+1]
+    return newList
+
+def calcLantern(population):
+    sumPop = 0
+    for el in population:
+        sumPop += el
+    print(sumPop)
+
+def part2():
+    ageList = countAge(readInput("2021/2021_06.txt"))
+    for i in range(CHECK_DATE_2):
+        ageList = shiftAges(ageList)
+    calcLantern(ageList)
+
 if __name__ == '__main__':
           part1()
+          part2()
