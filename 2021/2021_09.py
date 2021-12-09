aebcb3d71aad717ce92b6240a0a5412017d61212
HOHENSTEIN Christian <Christian.HOHENSTEIN@akka.eu>
2021-12-09 20:08:53 +0100

    2021 December 09 Part 2 solved

--- 2021/2021_09.py
+++ 2021/2021_09.py
@@ -1,6 +1,7 @@
 TEST_INPUT = "2021/test.txt"
 PUZZLE_INPUT = "2021/2021_09.txt"
-
+checkedPos = []
+count = 0
 def readInput(filename):
     data = []
     init = True
@@ -31,6 +32,7 @@ def prMatrix(matrix):
 def getLowPoints(matrix):
     # prMatrix(matrix)
     lowPoints = []
+    lowCoordinates = []
     # exclude "10" top and bottom borders in analyzing
     for i in range(len(matrix)-1):
         # exclude "10" left and right borders in analyzing
@@ -40,8 +42,10 @@ def getLowPoints(matrix):
                and matrix[i][j] < matrix[i+1][j] \
                and matrix[i][j] < matrix[i][j-1] \
                and matrix[i][j] < matrix[i][j+1]:
+                # low point detected, save value and Coordinates
                 lowPoints.append(matrix[i][j])
-    return(lowPoints)
+                lowCoordinates.append((i,j))
+    return(lowPoints,lowCoordinates)
 
 def calcSum1(points):
     total = 0
@@ -49,13 +53,63 @@ def calcSum1(points):
         total += val + 1
     return total
 
+
+def calcBasin(tu, matrix):
+    # global list of already checked positions needed to avoid endless loop
+    global checkedPos
+    # store coordinates in variables for easier use
+    y = tu[0]
+    x = tu[1]
+    # calculate coordinates of adjacent fields
+    top = (y+1,x)
+    right = (y,x+1)
+    left = (y,x-1)
+    bot = (y-1,x)
+    # store adjacent fields in list
+    dirIDs = [top,right,bot,left]
+    if matrix[y][x] < 9:
+        # part of the basin detected
+        # store this field in global list of already checked fields
+        checkedPos.append(tu)
+        # found field so count is "1"
+        val = 1
+        for i in dirIDs:
+            if i not in checkedPos:
+                # increase count by every not already checked adjacent field
+                val += calcBasin(i, matrix)
+    else:
+        # border detected
+        val = 0
+    return val
+
+
+def getBasin(lowCoords, matrix):
+    basin = []
+    # run through all the earlier found coordinates (saved as tuples)
+    for t in lowCoords:
+        # initialize counter variable
+        val = 0
+        # start recursive check
+        val += calcBasin(t, matrix)
+        # append basin size in list
+        basin.append(val)
+    return(basin)
+
 def part1():
-    lowPoints = getLowPoints(readInput(PUZZLE_INPUT))
+    lowPoints = getLowPoints(readInput(PUZZLE_INPUT))[0]
     print("Part1:",calcSum1(lowPoints))
-    pass
+
 
 def part2():
-    pass
+    lowCoords = getLowPoints(readInput(PUZZLE_INPUT))[1]
+    basins = getBasin(lowCoords,readInput(PUZZLE_INPUT))
+    basins = sorted(basins)
+    totalPart2 = 1
+    # multiply last 3 elements in sorted list
+    for spot in basins[-3:]:
+        totalPart2 *= spot
+    print("Part2:",totalPart2)
+
 
 if __name__ == '__main__':
     part1()
